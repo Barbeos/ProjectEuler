291c79b3077bb935bf0f1f3ced27632d3d659417
Barbeos <32571022+Barbeos@users.noreply.github.com>
2019-04-07 09:40:17 +0200

    problem 16

---
+++ fifth/fifth/11_20/Problem16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace fifth._11_20
+{
+    class Problem16
+    {
+        public static BigInteger SumOfNumber()
+        {
+            int result = 0;
+            foreach (char s in PowOfFifteen().ToCharArray()) {
+                
+                result += (int)Char.GetNumericValue(s);
+            }            
+            return result;
+        }
+
+        private static String PowOfFifteen()
+        {
+            return BigInteger.Pow(2, 1000).ToString();
+        }
+    }
+}
---
+++ fifth/fifth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fifth._11_20;
+
+namespace fifth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Problem16.SumOfNumber());
+            Console.ReadKey();
+        }
+    }
+}
