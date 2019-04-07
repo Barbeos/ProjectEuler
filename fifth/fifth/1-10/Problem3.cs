251fd641828189b5a9b49993507c6e530361e32f
Barbeos <32571022+Barbeos@users.noreply.github.com>
2019-04-07 23:14:42 +0200

    Problem 3 done

--- fifth/fifth/1-10/Problem3.cs
+++ fifth/fifth/1-10/Problem3.cs
@@ -8,14 +8,33 @@ namespace fifth._1_10
 {
     class Problem3
     {
-        public static List<long> PrimeNumbers { get; set; }
-
-        public static long LargestPrimeFactor()
+        public static int LargestPrimeFactor()
         {
-            long result = 0;
+            int result = 0;
+            long number = 600851475143;
 
-
+            for(long i = (long)Math.Ceiling(Math.Sqrt(number)); i > 0; i -= 2)
+            {
+                if (number % i == 0)
+                    if (IsPrime(i))
+                        return (int)i;                        
+            }
             return result;
+        }
+
+        public static bool IsPrime(long factor)
+        {
+            if (factor <= 1) return false;
+            if (factor == 2) return true;
+            if (factor % 2 == 0) return false;
+
+            long boundary = (long)Math.Floor(Math.Sqrt(factor));
+
+            for (long i = 3; i <= boundary; i += 2)
+                if (factor % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
