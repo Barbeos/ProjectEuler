00a342d84c35d9ac4b5b11b3029347b298a73835
Alex josefsen <alexjosef@hotmail.com>
2019-04-08 16:04:07 +0200

    Problem 15 done

---
+++ fifth/fifth/11-20/Problem15.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifth._11_20
+{
+    class Problem15
+    {
+        public static long LatticePaths()
+        {
+            long result = 1;
+
+            int gridSize = 20;
+
+            for(int i = 0; i < gridSize; i++)
+            {
+                result = result * ((2 * gridSize) - i);
+                result = result / (i + 1);
+            }
+            return result;
+        }
+    }
+}
